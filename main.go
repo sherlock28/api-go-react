2b7af5d44a48e9131bc45237c2dccf5234c8a9d0
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-08-14 21:29:21 -0300

    Added: Port for Heroku

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Static("/", "./client/dist")
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "List of users",
+		})
+	})
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	app.Listen(":" + port)
+	fmt.Println("Server on port " + port)
 }
